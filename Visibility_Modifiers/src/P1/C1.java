369312d4a2ea585587a2d60468617ad64443e6a5
Ponhrith <ponhrith.nhep@gmail.com>
2023-02-08 09:57:56 +0700

    Testing

---
+++ Visibility_Modifiers/src/P1/C1.java
@@ -0,0 +1,25 @@
+package P1;
+
+public class C1 {
+    public int x;
+    int y;
+    private int z;
+
+    public int getZ(){
+        return z;
+    }
+
+    public void setZ(int z){
+        this.z = z;
+    }
+
+    public void m1(){
+
+    }
+    void m2(){
+
+    }
+    private void m3(){
+
+    }
+}
---
+++ Visibility_Modifiers/src/P1/C2.java
@@ -0,0 +1,13 @@
+package P1;
+
+import P1.C1;
+
+public class C2 {
+    public static void main(String[] args){
+        C1 object = new C1();
+        object.x=1;
+        object.y =2;
+        object.getZ();
+
+    }
+}
